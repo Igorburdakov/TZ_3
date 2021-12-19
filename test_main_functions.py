6228b191764adc9455c897c01da0b9095602ffac
Igorburdakov <96357112+Igorburdakov@users.noreply.github.com>
2021-12-19 20:23:35 +0300

    Add files via upload

---
+++ test_main_functions.py
@@ -0,0 +1,45 @@
+import unittest
+import random
+from Main_programm_TP_3 import num_min
+from Main_programm_TP_3 import num_max
+from Main_programm_TP_3 import num_sum
+from Main_programm_TP_3 import num_multiply
+
+
+class NumbersTest(unittest.TestCase):
+
+    def test_min(self):
+        a = []
+        for i in range(10000):
+            a.append(random.randrange(1, 100000))
+        with self.subTest(i=a):
+            self.assertEqual(min(a), num_min(a))
+
+    def test_max(self):
+        a = []
+        for i in range(10000):
+            a.append(random.randrange(1, 100000))
+        with self.subTest(i=a):
+            self.assertEqual(max(a), num_max(a))
+
+    def test_sum(self):
+        a = []
+        for i in range(10000):
+            a.append(random.randrange(1, 100000))
+        print(a)
+        with self.subTest(i=a):
+            self.assertEqual(sum(a), num_sum(a))
+
+    def test_multiply(self):
+        a = []
+        v = 1
+        for j in range(10):
+            a.append(random.randrange(1, 100))
+        for i in a:
+            v *= i
+        with self.subTest(i=a):
+            self.assertEqual(v, num_multiply(a))
+
+
+if __name__ == '__main__':
+    unittest.main()
---
+++ test_time_main_programm.py
@@ -0,0 +1,41 @@
+import unittest
+import time
+from Main_programm_TP_3 import pro_on
+
+
+class NumbersTest(unittest.TestCase):
+
+    def test_time(self):
+
+        start1 = time.process_time()
+        pro_on("file numbers.txt")
+        end1 = time.process_time() - start1
+
+        start2 = time.process_time()
+        pro_on("test2 numbers.txt")
+        end2 = time.process_time() - start2
+
+        self.assertLess(end2 / 1.45, end1)
+
+    def test_max_time(self):
+        start1 = time.process_time()
+        pro_on("test2 numbers.txt")
+        end1 = time.process_time() - start1
+        a = pro_on("test2 numbers.txt")
+        start3 = time.process_time()
+        min(a[4])
+        max(a[4])
+        v = 0
+        for i in a[4]:
+            i = int(i)
+            v += i
+        v = 1
+        for i in a[4]:
+            i = int(i)
+            v *= i
+        end3 = time.process_time() - start3
+        self.assertLess(end1, end3)
+
+
+if __name__ == '__main__':
+    unittest.main()
