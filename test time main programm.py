a4268241b86cbc434588e68e529342bbff25a0a9
Igorburdakov <96357112+Igorburdakov@users.noreply.github.com>
2021-12-19 15:32:53 +0300

    Update test time main programm.py

--- test time main programm.py
+++ test time main programm.py
@@ -17,11 +17,24 @@ class NumbersTest(unittest.TestCase):
 
         self.assertLess(end2 / 1.45, end1)
 
-    def test_max_time(self):
-        start1 = time.time()
+        def test_max_time(self):
+        start1 = time.process_time()
         pro_on("test2 numbers.txt")
-        end1 = time.time() - start1
-        self.assertLess(end1, 10)
+        end1 = time.process_time() - start1
+        a = pro_on("test2 numbers.txt")
+        start3 = time.process_time()
+        min(a[4])
+        max(a[4])
+        v = 0
+        for i in a[4]:
+            i = int(i)
+            v += i
+        v = 1
+        for i in a[4]:
+            i = int(i)
+            v *= i
+        end3 = time.process_time() - start3
+        self.assertLess(end1, end3)
 
 
 if __name__ == '__main__':
